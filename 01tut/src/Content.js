747a4fbb1838f97860b0d27375adb03c2cff5905
switbartlomiej@gmail.com <switbartlomiej@gmail.com>
2024-01-03 00:20:15 +0100

    End button

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Content = () => 
 {
+    const [name, setName] = useState('Bartek');
+    const [count, setCount] = useState(0);
+
     const handleNameChange = ()=> 
     {
       const names = ['Bob','Kevin','Bartek'];
       const int = Math.floor(Math.random() *3);
-      return names [int];
+      setName( names [int]);
     }
 
     const handleClick = () =>{
-        console.log('Ty fujaro popsułeś mi apke')
+        setCount(count+1)
+        console.log(count)
     }
-    const handleClick2 = (name) =>{
-        console.log(`${name} was clicked`)
+    const handleClick2 = () =>{
+        console.log(count)
     }
     const handleClick3 = (e) =>{
-        console.log(e)
+        console.log(e.target.innerText)
     }
 
   return (
@@ -24,12 +29,12 @@ const Content = () =>
     
     <main>
 
-        <p>
-          Hellow {handleNameChange()} !
+        <p onDoubleClick={handleClick}>
+          Hello {name} !
         </p>
+        <button onClick={handleNameChange}>Zmień imię</button>
         <button onClick={handleClick}>Click It</button>
-        <button onClick={() => handleClick2('Bartek')}>Click It</button>
-        <button onClick={(e) => handleClick3(e)}>Click It</button>
+        <button onClick={handleClick2}>Click It</button>
 
     </main>
   );
